076daada769eefea7c2f209afb8cd35d5ea5ec4b
Marlboromjh <273351558@qq.com>
2018-03-28 22:06:25 +0800

    Update practice.js

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,23 @@
 function count_same_elements(collection) {
   //在这里写入代码
+var obj={};
+var arr=[];
+//遍历数组
+for(i=0;i<collection.length;i++){
+    //若a中元素第一次出现，则
+    if(!obj[collection[i]]){   //a[i]就是对象obj的一个属性，obj[a[i]]就是属性值
+        obj[collection[i]]=1;//若第一次出现，次数记为1
+    }else{
+        obj[collection[i]]++;//次数加1
+    }
+}
+//遍历对象
+for(var i in obj){  
+   //console.log(i);//json对象的key  
+   //console.log(obj[i]);//json对象的值
+   arr.push({key:i,count:obj[i]}); 
+}  
+return arr;
 }
 
 module.exports = count_same_elements;
