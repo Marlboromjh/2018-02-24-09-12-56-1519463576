9028cc1429613fcaf57769606b8ef1511225547f
Marlboromjh <273351558@qq.com>
2018-03-28 10:51:43 +0800

    Update practice.js

--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,17 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  /*for(var prop in collection_b){
+    console.log(collection_b[prop]);
+}*/
+var arr=[];
+for(var i=0;i<collection_a.length;i++){
+    for(var j=0;j<collection_b.value.length;j++){
+        if(collection_a[i]==collection_b.value[j]){
+            arr.push(collection_a[i]);
+        }
+    }
+}
+return arr;
 }
 
 module.exports = collect_same_elements;
