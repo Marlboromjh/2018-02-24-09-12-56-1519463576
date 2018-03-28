56b9431bcb7e62e1eb4a618dfd78695161cf1bf8
Marlboromjh <273351558@qq.com>
2018-03-28 10:37:26 +0800

    Update practice.js

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -1,12 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var arr=[];//定义一个空数组接收相同的元素
   for(i=0;i<collection_a.length;i++){
       for(j=0;j<collection_b.length;j++){
          if(collection_a[i]==collection_b[j]){
-             return collection_a[i];
+             arr.push(collection_a[i]);
          } 
       }
   }
+  return arr;
 }
 
 module.exports = collect_same_elements;
