bf6558d716a8046ce3de211bfa2cae4e53653eab
Marlboromjh <273351558@qq.com>
2018-04-01 22:31:50 +0800

    Update collect_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -2,6 +2,14 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var collection_a = [1, 2, 3, 4, 5];
+var collect_all_even=[];
+for(var i=0;i<collection_a.length;i++){
+    if(collection_a[i]%2==0){
+        collect_all_even.push(collection_a[i]);
+    }
+}
+  return collect_all_even;
 }
 
 module.exports = collect_all_even;
